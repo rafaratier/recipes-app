d6bd783c585ffe0e3c19ed9d7ab66607cdc38bb7
Rafael <rafaelratier@hotmail.com>
2022-03-29 17:44:24 -0300

    fetch da receita com parametro vindo da url completo

--- src/components/FoodDetails.js
+++ src/components/FoodDetails.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { getRecipe } from '../helpers/fetchFoodRecipes';
 
 function FoodDetails() {
+  const { id } = useParams();
+
+  const [recipe, setRecipe] = useState();
+
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      const result = await getRecipe(id);
+      setRecipe(result);
+    };
+    fetchRecipe();
+  }, [id]);
+
+  console.log(recipe);
+
   return (
     <div>
       <h1>DETALHES DA COMIDA</h1>
--- src/helpers/fetchFoodRecipes.js
+++ src/helpers/fetchFoodRecipes.js
@@ -15,3 +15,9 @@ export const getFoodCategories = async () => {
   const result = await request.json();
   return result;
 };
+
+export const getRecipe = async (recipeId) => {
+  const request = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeId}`);
+  const result = await request.json();
+  return result;
+};
