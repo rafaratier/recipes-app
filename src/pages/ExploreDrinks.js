6e5805f80892db5a53a639cf18f88ebba705a3b8
carlatakagi <carlatakagi@gmail.com>
2022-03-29 22:51:04 -0400

    telas de explorar comida e bebida

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -1,13 +1,39 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import FooterMenu from '../components/FooterMenu';
 
 function ExploreDrinks() {
+  const history = useHistory();
+
   return (
     <div>
       <h1>pagina de explorar bebida</h1>
+
+      <div>
+        <button
+          type="submit"
+          data-testid="explore-by-nationality"
+          onClick={ () => history.push('/explore/drinks/ingredients') }
+        >
+          By Ingredient
+
+        </button>
+        <button
+          type="submit"
+          data-testid="explore-surprise"
+        >
+          Surprise me!
+
+        </button>
+      </div>
       <FooterMenu />
     </div>
   );
 }
 
 export default ExploreDrinks;
+
+/*
+Ao clicar no botão "Surprise me!" da tela de explorar bebidas a rota muda para a
+página de detalhes de uma bebida aleatória obtida através do endpoint https://www.thecocktaildb.com/api/json/v1/1/random.php.
+*/
--- src/pages/ExploreFoods.js
+++ src/pages/ExploreFoods.js
@@ -1,13 +1,48 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import FooterMenu from '../components/FooterMenu';
 
 function ExploreFoods() {
+  const history = useHistory();
+
   return (
     <div>
       <h1>pagina de explorar comida</h1>
+
+      <div>
+        <button
+          type="submit"
+          data-testid="explore-by-ingredient"
+          onClick={ () => history.push('/explore/foods/ingredients') }
+        >
+          By Ingredient
+
+        </button>
+        <button
+          type="submit"
+          data-testid="explore-by-nationality"
+          onClick={ () => history.push('/explore/foods/nationalities') }
+        >
+          By Nationality
+
+        </button>
+        <button
+          type="submit"
+          data-testid="explore-surprise"
+          onClick={ () => history.push('/explore/foods') }
+        >
+          Surprise me!
+
+        </button>
+      </div>
       <FooterMenu />
     </div>
   );
 }
 
 export default ExploreFoods;
+
+/*
+Ao clicar no botão "Surprise me!" da tela de explorar comidas a rota muda para a
+página de detalhes de uma comida aleatória obtida através do endpoint https://www.themealdb.com/api/json/v1/1/random.php;
+*/
